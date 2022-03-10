d191582ca16a7b867b6fc7c82e5c78bad7f8b5e8
lahari <laharigaddam17@gmail.com>
2022-03-10 20:43:53 +0530

    push calc and test code

--- src/main/java/calc.java
+++ src/main/java/calc.java
@@ -1,5 +1,18 @@
 public class calc {
-    public int add(int a, int b) {
+    public int addition(int a, int b) {
         return a+b;
     }
+    public int subtraction(int a, int b) {
+        return a-b;
+    }
+    public int multiplication(int a, int b) {
+        return a*b;
+    }
+    public int division(int a, int b) {
+        if(b==0){
+            throw new IllegalArgumentException("Given number can not be divided by 0!");
+        }
+        return a/b;
+    }
+
 }
--- src/test/java/calcTest.java
+++ src/test/java/calcTest.java
@@ -6,8 +6,24 @@ public class calcTest {
     @Test
     public void add_test(){
         calc a = new calc();
-        assertEquals(4,a.add(2,2));
+        assertEquals(4,a.addition(2,2));
     }
+    @Test
+    public void sub_test(){
+        calc b = new calc();
+        assertEquals(2,b.subtraction(6,4));
+    }
+    @Test
+    public void multi_test(){
+        calc c = new calc();
+        assertEquals(8, c.multiplication(4,2));
+    }
+    @Test
+    public void div_test(){
+        calc d = new calc();
+        assertEquals(6, d.division(30,5));
+    }
+
 
 
 }
